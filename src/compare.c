7497549309cdb1778ac4a8ccd00751557ae57315
iluvpy <miomanns265@gmail.com>
2021-11-22 23:53:47 +0100

    working on compare

--- src/compare.c
+++ src/compare.c
@@ -1,6 +1,22 @@
 #include "compare.h"
-
+#include <stdlib.h>
 
 char *compare(char *str1, char *str2) {
-	
+	size_t len1 = strlen(str1);	
+	size_t len2 = strlen(str2);	
+	// size_t result_len = LEN;
+	// char *result = malloc(result_len);
+	// int i = 0;
+	// for (;;) {
+	// 	if (i < len1 && i < len2) {
+	// 		if (str1[i] != str2[i]) {
+
+	// 		}	
+	// 	}
+	// 	i++;
+	// }
+
+	result = realloc(result, strlen(result)+1);
+	result[strlen(result)+1] = '\0';
+	return result;
 }
--- src/include/compare.h
+++ src/include/compare.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define LEN 512
+
 // returns a string that shows the differences between str1 and str2
 // the string returned should be printed to the console 
 char *compare(char *str1, char *str2);
